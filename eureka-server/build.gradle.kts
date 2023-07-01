40cd3cf68de096d9e7f925a9e2af45ef1e0fe846
fredisooon <fyodorstrujanov26@gmail.com>
2023-07-01 19:40:19 +0300

    добавить базовые модули микросервисной архитектуры: eureka-server, eureka-client

---
+++ eureka-server/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.1"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "ru.tinkoff.summer"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2022.0.3"
+
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,4 @@
 rootProject.name = "spring-micro-demo"
 
+includeBuild("eureka-server")
+includeBuild("eureka-client")
