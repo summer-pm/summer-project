a41a5c173e567af667e8cc584250923ae1886163
Nikita Vashkulatov <nikitavaskulatov@gmail.com>
2023-07-08 18:38:32 +0000

    Добавил task-service и docker-compose с kafka

---
+++ task-microservice/src/main/java/ru/tinkoff/summer/taskmicroservice/TaskMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.summer.taskmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMicroserviceApplication.class, args);
+    }
+
+}
