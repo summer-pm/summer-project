fb6108f0b7a292ab0d55657f870f1c5fff64837c
Nikita Vashkulatov <vashkulatov.nikita@promregion.net>
2023-07-14 13:49:20 +0000

    add choose language

--- frontend/src/components/task/CodeEditor.vue
+++ frontend/src/components/task/CodeEditor.vue
@@ -1,29 +1,34 @@
 <template>
   <div>
+    <select v-model="solution.language">
+      <option value="JAVA">Java</option>
+      <option value="PYTHON">Python</option>
+    </select>
     <codemirror
+        v-if="!loading"
         v-model="solution.code"
         :autofocus="true"
+        :disabled="$props.pending"
         :extensions="extensions"
         :indent-with-tab="true"
-        :style="{ height: '400px', width: '100%' }"
+        :style="{ height: '100%', width: '100%' }"
         :tab-size="4"
-        :disabled="$props.pending"
         placeholder="Ваше решение..."
     />
     <button :disabled="$props.pending" @click="sendSolution">
       <span v-if="!$props.pending"> Отправить </span>
-      </button>
+    </button>
   </div>
 
 </template>
 
 <script>
-import {defineComponent, reactive, shallowRef} from "vue";
+import {defineComponent, reactive, ref, shallowRef, watch} from "vue";
 import {Codemirror} from "vue-codemirror";
 import {java} from "@codemirror/lang-java";
-import {python} from "@codemirror/lang-python";
 import {useRoute} from "vue-router";
 import {oneDark} from "@codemirror/theme-one-dark";
+import {python} from "@codemirror/lang-python";
 
 
 export default defineComponent({
@@ -31,14 +36,18 @@ export default defineComponent({
   props: {
     pending: Boolean,
   },
-  setup(props) {
-    const extensions =  [ java()]
 
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches)
-        extensions.push(oneDark)
-    // Codemirror EditorView instance ref
-    const view = shallowRef()
+  setup(props) {
+    const loading = shallowRef(false)
     const route = useRoute()
+
+    const extensions = ref([java()])
+
+
+
+    checkForDarkTheme();
+
+    const view = shallowRef()
     const handleReady = (payload) => {
       view.value = payload.view
     }
@@ -56,8 +65,28 @@ export default defineComponent({
     function sendSolution() {
       this.$emit('sendAttempt', solution)
     }
+     function checkForDarkTheme() {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+        extensions.value.push(oneDark)
+    }
+
+    watch(
+        () => solution.language,
+        (lang) => {
+          loading.value = true;
+          if (lang === 'JAVA') {
+            extensions.value = [java()]
+          } else {
+            extensions.value = [python()]
+          }
+         checkForDarkTheme();
+          setTimeout(() => loading.value = false, 200)
+
+        }
+    )
 
     return {
+      loading,
       sendSolution,
       solution,
       extensions,
@@ -68,13 +97,14 @@ export default defineComponent({
 })
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 button {
   margin-top: 40px;
   background: var(--color-main);
   color: var(--color-text);
   padding: 10px 40px;
-  border-radius: 10px;
+  border-radius: 100px;
+  box-shadow: 0px 5px 15px 0px rgba(14, 26, 57, 0.20);
   border: none;
   font-size: 16px;
   cursor: pointer;
@@ -84,4 +114,21 @@ button {
     transform: scale(102%);
   }
 }
+select{
+
+  margin-bottom: 35px;
+  font-size: 16px;
+  padding: 10px 50px;
+   border-radius: 100px;
+  border: none;
+  color: var(--color-text);
+  background-color: var(--color-background-soft);
+}
+
+
+
+
+/* скроет иконку стрелки в IE */
+
+
 </style>
---
+++ frontend/src/components/task/ResultDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "ResultDisplay"
+}
+</script>
+
+<style scoped>
+
+</style>
