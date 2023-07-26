575a399bb80da0a2b5965e28d2a36c5a69ee6cae
Nikita <78970794+R3nnyWeb@users.noreply.github.com>
2023-07-26 16:26:53 +0300

    Update install-serts.sh

--- frontend/install-serts.sh
+++ frontend/install-serts.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+#certbot --nginx -v --domains beercode.ru --agree-tos --email nikitavaskulatov@gmail.com --non-interactive
 nginx 
-certbot --nginx -v --domains beercode.ru --agree-tos --email nikitavaskulatov@gmail.com --non-interactive
+
