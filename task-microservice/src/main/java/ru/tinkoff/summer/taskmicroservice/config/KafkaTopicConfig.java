c8115a005f43239fcf1b1a85134b61c05dfe169d
Nikita Vashkulatov <vashkulatov.nikita@promregion.net>
2023-07-27 16:08:34 +0300

    hotfix

--- task-microservice/src/main/java/ru/tinkoff/summer/taskmicroservice/config/KafkaTopicConfig.java
+++ task-microservice/src/main/java/ru/tinkoff/summer/taskmicroservice/config/KafkaTopicConfig.java
@@ -1,10 +1,14 @@
 package ru.tinkoff.summer.taskmicroservice.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+
+
 
 
 import static ru.tinkoff.summer.taskshareddomain.ConnectionConstants.*;
@@ -12,13 +16,15 @@ import static ru.tinkoff.summer.taskshareddomain.ConnectionConstants.*;
 @Configuration
 public class KafkaTopicConfig {
 
-
+    Logger logger = LoggerFactory.getLogger(KafkaTopicConfig.class);
     @Bean
     public NewTopic attemptTopic(){
+        logger.error("Atttempts topics = {}" ,ATTEMPT_TOPIC_PARTITIONS);
         return TopicBuilder.name(ATTEMPT_TOPIC_NAME).partitions(ATTEMPT_TOPIC_PARTITIONS).build();
     }
     @Bean
     public NewTopic resultTopic(){
+        logger.error("Result topics = {}" ,RESULT_TOPIC_PARTITIONS);
         return TopicBuilder.name(RESULT_TOPIC_NAME).partitions(RESULT_TOPIC_PARTITIONS).build();
     }
 }
